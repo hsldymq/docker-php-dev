125716e35edab4a9557b94840edc9008548cc229
hsldymq <hsldymq@qq.com>
2017-10-23 19:08:44 +0800

    调整启动脚本

--- 7.1-fpm/build/scripts/docker_start.sh
+++ 7.1-fpm/build/scripts/docker_start.sh
@@ -1,15 +1,38 @@
  #!/bin/sh
 
-if [ ${SYSLOG} = "on" ]; then
-    if [ ${SYSLOG_REMOTE_ADDR} ]; then
-        SLR_ADDR=$SYSLOG_REMOTE_ADDR
-        if [ ${SYSLOG_REMOTE_PORT} ]; then
-            SLR_PORT=$SYSLOG_REMOTE_PORT
-        else
-            SLR_PORT=9997
-        fi
-    fi
-    rsyslogd
-fi
+function start() {
+    prepareDirectories
+    runSyslog
+    php-fpm
+} 
 
- php-fpm
+function prepareDirectories() {
+    prepareDirFromEnv "XDEBUG_PROFILER_DIR"
+    prepareDirFromEnv "XDEBUG_TRACE_DIR"
+    prepareDirFromEnv "XHPROFILE_DIR"
+}
+
+function prepareDirFromEnv() {
+    eval DIR=\$$1
+
+    if [ $DIR ]; then
+        mkdir -vp $DIR
+        chmod 777 $DIR
+    fi
+}
+
+function runSyslog() {
+    if [ "${SYSLOG}" = "on" ]; then
+        if [ $SYSLOG_REMOTE_ADDR ]; then
+            SLR_ADDR=$SYSLOG_REMOTE_ADDR
+            if [ $SYSLOG_REMOTE_PORT ]; then
+                SLR_PORT=$SYSLOG_REMOTE_PORT
+            else
+                SLR_PORT=9997
+            fi
+        fi
+        rsyslogd
+    fi
+}
+
+start
