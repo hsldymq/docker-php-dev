9579f7947258924f3961d9ebbe3901b7cae5f4c0
hsldymq <hsldymq@qq.com>
2019-12-13 18:46:45 +0800

    update script

--- scripts/php/list_extensions.php
+++ scripts/php/list_extensions.php
@@ -5,7 +5,7 @@ $exts = array_merge(get_loaded_extensions(false));
 natcasesort($exts);
 
 foreach ($exts as $each) {
-    echo $each;
+    echo "* {$each}";
     if ($showVersion) {
         echo ": ".phpversion($each).PHP_EOL;
     } else {
